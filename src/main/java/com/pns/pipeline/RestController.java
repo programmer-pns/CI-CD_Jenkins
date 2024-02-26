c1b009941456851a44c66fe37eece07a8518ec6a
Priyabrata Nayak <npriyabrata120@gmail.com>
2024-02-26 23:11:31 +0530

    Test Commit

--- src/main/java/com/pns/pipeline/RestController.java
+++ src/main/java/com/pns/pipeline/RestController.java
@@ -15,6 +15,6 @@ public class RestController {
 
     @GetMapping("/")
     public String home(){
-        return "Welcome to Pipeline Homepage";
+        return "Welcome to Pipeline Homepage: "+LocalDateTime.now();
     }
 }
