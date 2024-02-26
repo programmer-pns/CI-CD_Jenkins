783be38861c1e54ac2b44a10c8801cf3a31914f6
Priyabrata Nayak <npriyabrata120@gmail.com>
2024-02-26 18:07:14 +0530

    Testing

--- src/main/java/com/pns/pipeline/RestController.java
+++ src/main/java/com/pns/pipeline/RestController.java
@@ -15,6 +15,6 @@ public class RestController {
 
     @GetMapping("/")
     public String home(){
-        return "Welcome to Pipeline Testing Home Page with Jenkins";
+        return "Welcome to Pipeline Testing Home Page Test";
     }
 }
