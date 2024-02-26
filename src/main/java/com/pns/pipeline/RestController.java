a7dfbf92148566905facbda19a69d3b4933c1dce
Priyabrata Nayak <npriyabrata120@gmail.com>
2024-02-26 15:58:28 +0530

    Added rest controller

--- src/main/java/com/pns/pipeline/RestController.java
+++ src/main/java/com/pns/pipeline/RestController.java
@@ -1,4 +1,16 @@
 package com.pns.pipeline;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@org.springframework.web.bind.annotation.RestController
 public class RestController {
+
+    @GetMapping("/")
+    public String healthCheck(){
+        return String.format("\"time\":\"%s\"", LocalDateTime.now());
+    }
+
 }
